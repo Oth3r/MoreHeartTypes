ceb2702bae209da3c9a16cba19a8cbfb3b47c65d
Oth3r <oth3rone@outlook.com>
2024-06-16 13:12:37 -0500

    1.19.4 port

--- fabric/src/main/java/one/oth3r/more_heart_types/mixin/DrawHeartsMixin.java
+++ fabric/src/main/java/one/oth3r/more_heart_types/mixin/DrawHeartsMixin.java
@@ -1,8 +1,10 @@
 package one.oth3r.more_heart_types.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
@@ -11,17 +13,18 @@ import one.oth3r.more_heart_types.MoreHeartTypes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public class DrawHeartsMixin {
+public class DrawHeartsMixin extends DrawableHelper {
 
     @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "drawHeart", at = @At(value = "HEAD"), cancellable = true)
-    private void drawHeart(DrawContext context, InGameHud.HeartType type, int x, int y, int v, boolean blinking, boolean half, CallbackInfo ci) {
+    private void drawHeart(MatrixStack matrices, InGameHud.HeartType type, int x, int y, int v, boolean blinking, boolean half, CallbackInfo ci) {
         // only run if normal heart or a container heart
         if (!(type.equals(InGameHud.HeartType.NORMAL) || type.equals(InGameHud.HeartType.CONTAINER))) return;
         // add a container boolean for when the heart type is a container
@@ -39,8 +42,7 @@ public class DrawHeartsMixin {
                 texture = new Identifier("textures/gui/sprites/hud/heart/starve_container.png");
                 if (blinking) texture = new Identifier("textures/gui/sprites/hud/heart/starve_container_blinking.png");
             }
-            context.drawTexture(texture, x, y,0,0, 9, 9,9,9);
-            ci.cancel();
+            draw(texture,ci,matrices,x,y);
         }
 
         // only if the player has taken damage in the last 20 ticks
@@ -63,8 +65,7 @@ public class DrawHeartsMixin {
                 texture = new Identifier("textures/gui/sprites/hud/heart/thorns_container.png");
                 if (blinking) texture = new Identifier("textures/gui/sprites/hud/heart/thorns_container_blinking.png");
             }
-            context.drawTexture(texture, x, y,0,0, 9, 9,9,9);
-            ci.cancel();
+            draw(texture,ci,matrices,x,y);
         }
 
         // suffocation
@@ -72,8 +73,7 @@ public class DrawHeartsMixin {
             Identifier texture = new Identifier("textures/gui/sprites/hud/heart/suffocate_full.png");
             if (half) texture = new Identifier("textures/gui/sprites/hud/heart/suffocate_half.png");
             if (container) return;
-            context.drawTexture(texture, x, y,0,0, 9, 9,9,9);
-            ci.cancel();
+            draw(texture,ci,matrices,x,y);
         }
 
         // drowning
@@ -81,8 +81,7 @@ public class DrawHeartsMixin {
             Identifier texture = new Identifier("textures/gui/sprites/hud/heart/drown_full.png");
             if (half) texture = new Identifier("textures/gui/sprites/hud/heart/drown_half.png");
             if (container) return;
-            context.drawTexture(texture, x, y,0,0, 9, 9,9,9);
-            ci.cancel();
+            draw(texture,ci,matrices,x,y);
         }
 
         // void
@@ -90,8 +89,7 @@ public class DrawHeartsMixin {
             Identifier texture = new Identifier("textures/gui/sprites/hud/heart/void_full.png");
             if (half) texture = new Identifier("textures/gui/sprites/hud/heart/void_half.png");
             if (container) return;
-            context.drawTexture(texture, x, y,0,0, 9, 9,9,9);
-            ci.cancel();
+            draw(texture,ci,matrices,x,y);
         }
 
         // fire / lava / campfire / magma
@@ -103,8 +101,18 @@ public class DrawHeartsMixin {
             if (half) texture = new Identifier("textures/gui/sprites/hud/heart/fire_half.png");
             if (container) return;
             // not animated for some reason,,,
-            context.drawTexture(texture, x, y,0,0, 9, 9, 9, 72);
-            ci.cancel();
+            draw(texture,ci,matrices,x,y,9,72);
         }
+    }
+
+    @Unique private static void draw(Identifier texture, CallbackInfo ci, MatrixStack matrices, int x, int y) {
+        draw(texture,ci,matrices,x,y,9,9);
+    }
+
+    @Unique private static void draw(Identifier texture, CallbackInfo ci, MatrixStack matrices, int x, int y, int textureX, int textureY) {
+        RenderSystem.setShaderTexture(0, texture);
+        drawTexture(matrices, x, y, 0, 0, 9, 9,9,textureX,textureY);
+        RenderSystem.setShaderTexture(0, DrawableHelper.GUI_ICONS_TEXTURE);
+        ci.cancel();
     }
 }
